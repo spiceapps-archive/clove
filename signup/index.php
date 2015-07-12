cba05ceadb06a357dc1567e823650291d13d3161
Tim Erickson <tim@neutyp.com>
2015-07-12 03:53:58 -0700

    Final backup from 2013 (actual changes 2011)
    "Public Beta 1 is currently closed"

--- signup/index.php
+++ signup/index.php
@@ -1,3 +1,43 @@
+<?php
+
+
+$method = $_POST;
+
+
+if($method)
+{
+	require_once dirname(__FILE__)."/../spice/apps/clove/services/public/CloveUserService.php";
+	
+	$name	   = $method["name"];
+	$user	   = $method["email"];
+	$invite    = $method["code"];	
+	$password  = $method["password"];
+	$subscribe = array_key_exists("subscribe",$method);
+	
+	
+	
+	$service = new CloveUserService();
+	
+	$m = "Location: signup/";
+		
+
+	$result = $service->register($name,$user,$password,$invite,$subscribe);
+	
+	
+	$e = $result->errorCode;
+	
+	if(!$e)
+	{
+		$m = $result->message;
+	}
+	else
+	{
+		$m = implode("<br />",Logger::getLogs());
+	}
+}
+
+
+?>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" media="screen" href="/backend/style.css" />
@@ -5,14 +45,14 @@
     </head>
     <body>
 
-        <div id="header"><a class="light" href="/">Registration &#9656;</a> Closed</div>
+        <div id="header"><a class="light" href="/">Registration &#9656;</a> <?=$e ? 'Error' : 'Success'; ?></div>
         
         <div id="content">
             
 		<br />
         
         <div class="<?=$e ? 'error' : 'tip'; ?>">
-			<b>Alert:</b> Public Beta 1 is currently closed! Thank you for your interest. Email hello@spiceapps.com if you're still interested in playing with the application.
+			<b>Alert:</b> <?=$m; ?>
 		</div>
 	
 		
